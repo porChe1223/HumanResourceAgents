4c99b6b729c81d5537579256d5eefd38ab709bbc
porChe1223 <houshinri@gmail.com>
2025-06-25 05:27:28 +0900

    update: chainlitを関数ベースのnodeに適応させる

--- src/main.py
+++ src/main.py
@@ -1,32 +1,29 @@
 import chainlit as cl
 from langchain_core.messages import HumanMessage
-from workflows.workflow import workflow
+from langchain_core.runnables import RunnableConfig
+from core.pipeline.pipeline import pipeline
 
 @cl.on_chat_start
 def on_chat_start():
     # チェーンをセッションに保存
-    cl.user_session.set("chain", workflow)
+    cl.user_session.set("chain", pipeline)
 
 @cl.on_message
-async def on_chat_message(message: cl.Message):
-    # チェーンを取得
-    chain = cl.user_session.get("chain")
+async def on_message(message: cl.Message):
+    config = {"configurable": {"thread_id": cl.context.session.id}}
+    cb = cl.AsyncLangchainCallbackHandler()
 
     try:
-        # チェーンを呼び出し
-        res = await chain.ainvoke(
+        res = await pipeline.ainvoke(
             {"messages": [HumanMessage(content=message.content)]},
-            config={"callbacks": [cl.AsyncLangchainCallbackHandler()]},
+            config=RunnableConfig(callbacks=[cb], **config),
         )
 
-        # チェーンの応答を送信
         await cl.Message(content=res["messages"][-1].content).send()
-
-        # pretty_print_message(res) # resはグラフの最終状態であり、単一メッセージではないため、この関数は期待通りに動作しません。
+        
     except Exception as e:
-        # リトライが最終的に失敗した場合、エラーをターミナルとUIに表示します
-        error_message = f"エラーが発生しました: \n{e}"
-        print(f"最終的なエラー: \n{error_message}")
-        await cl.Message(content=error_message).send()
+        await cl.Message(content=f"エラーが発生しました: {e}").send()
 
-
+@cl.on_chat_resume
+async def on_chat_resume(thread):
+    pass
