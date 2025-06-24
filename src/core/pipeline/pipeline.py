9e4b571cb076d65126aecd659f4c9c57204a0672
porChe1223 <houshinri@gmail.com>
2025-06-25 06:18:27 +0900

    update: 戦略決定エージェントをパイプラインに追加

--- src/core/pipeline/pipeline.py
+++ src/core/pipeline/pipeline.py
@@ -1,4 +1,5 @@
-from langgraph.graph import END, StateGraph, MessagesState, START
+from langgraph.graph import StateGraph, MessagesState, START, END
+from strategy.chain.strategy_chain import strategy_chain
 from research.chain.research_chain import research_chain
 from recommend.chain.recommend_chain import recommend_chain
 
@@ -10,13 +11,15 @@ pipeline = (
     StateGraph(MessagesState)
 
     # --- チェーンを定義 ---
+    .add_node("strategy_chain", strategy_chain)
     .add_node("research_chain", research_chain)
     .add_node("recommend_chain", recommend_chain)
 
     # --- チェーンを接続 ---
-    .add_edge(START, "research_chain")             # 開始 => 調査
-    .add_edge("research_chain", "recommend_chain") # 調査 => 推薦
-    .add_edge("recommend_chain", END)              # 推薦 => 終了
+    .add_edge(START, "strategy_chain")             # 開始     =>  戦略決定
+    .add_edge("strategy_chain", "research_chain")  # 戦略決定  =>  調査
+    .add_edge("research_chain", "recommend_chain") # 調査     =>  推薦
+    .add_edge("recommend_chain", END)              # 推薦     =>  終了
 
     # --- コンパイル ---
     .compile()
