38b69775a161344bcefeb0e21474e8e0da1dee8b
porChe1223 <houshinri@gmail.com>
2025-06-29 18:06:30 +0900

    update: テストデータ挿入からAIモデルを使えるように変更

--- src/strategy/func/analyze_skill.py
+++ src/strategy/func/analyze_skill.py
@@ -1,7 +1,7 @@
 from core.state.pipeline_state import PipelineState
 from core.helper.runnable_retry import runnable_retry
 from strategy.agent.analyze_skill_agent import analyze_skill_agent
-from shared.helper.read_text import read_text
+# from shared.helper.read_text import read_text
 
 def analyze_skill(state: PipelineState):
     """
@@ -10,13 +10,13 @@ def analyze_skill(state: PipelineState):
     - 要件から必要なスキルを分析
     """
     # スキル分析エージェント
-    #result = runnable_retry(analyze_skill_agent).invoke(state)
+    result = runnable_retry(analyze_skill_agent).invoke(state)
 
     # PipelineStateを更新
-    #user_input = result["messages"][0]       # ユーザ入力
-    user_input = read_text("../docs/sample/user_input/llm_developer.txt")
-    #skills = result["messages"][-1].content  # スキル
-    skills = read_text("../docs/sample/strategy/skill_analyze_result.txt")
+    user_input = result["messages"][0]       # ユーザ入力
+    skills = result["messages"][-1].content  # スキル
+    # user_input = read_text("../docs/sample/user_input/llm_developer.txt")
+    # skills = read_text("../docs/sample/strategy/skill_analyze_result.txt")
     sites = ""                               # サイト
     researchs = ""                           # 調査結果
 
