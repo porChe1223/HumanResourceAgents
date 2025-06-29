db93302d647bc58997e8b4a7fc6d269fc473fc6e
porChe1223 <houshinri@gmail.com>
2025-06-30 00:36:28 +0900

    tips: langchain-awsに必要な環境変数追加

--- src/core/config/set_env.py
+++ src/core/config/set_env.py
@@ -7,21 +7,33 @@ class SetEnv:
         OPENAI_API_KEY: OpenAIのAPIキー
         GOOGLE_API_KEY: GeminiのAPIキー
         TAVILY_API_KEY: TavilyのAPIキー
+        AWS_ACCESS_KEY_ID: AWSのアクセスキーID
+        AWS_SECRET_ACCESS_KEY: AWSのシークレットアクセスキー
+        AWS_SESSION_TOKEN: AWSのセッショントークン
+        AWS_REGION: AWSのリージョン
     """
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
     TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
+    AWS_REGION = os.getenv("AWS_REGION")
 
     @classmethod
     def set_env(self):
         """環境変数が設定されているかを確認"""
-        required_keys = [self.OPENAI_API_KEY, self.GOOGLE_API_KEY, self.TAVILY_API_KEY]
+        required_keys = [
+            self.OPENAI_API_KEY,
+            self.GOOGLE_API_KEY,
+            self.TAVILY_API_KEY,
+            self.AWS_ACCESS_KEY_ID,
+            self.AWS_SECRET_ACCESS_KEY,
+            self.AWS_SESSION_TOKEN,
+            self.AWS_REGION,
+        ]
 
-        if not required_keys[0]:
-            raise ValueError("OPENAI_API_KEYが設定されていません")
-        if not required_keys[1]:
-            raise ValueError("GOOGLE_API_KEYが設定されていません")
-        if not required_keys[2]:
-            raise ValueError("TAVILY_API_KEYが設定されていません")
-
+        for key in required_keys:
+            if key is None:
+                raise ValueError(f"{key}が設定されていません")
 
