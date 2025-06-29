99f20229e632763c4bb3ba343f6a3be1c3b3eda9
porChe1223 <houshinri@gmail.com>
2025-06-29 17:40:38 +0900

    fix: いきなりあるいは調査結果がたいして得られなかった場合、同じURLで調査に行かせる意味がないのでstrategy=>researchを絶対にする

--- src/orchestrate/agent/orchestrate_agent.py
+++ src/orchestrate/agent/orchestrate_agent.py
@@ -1,7 +1,6 @@
 from langgraph.prebuilt import create_react_agent
 from core.llm.openai import llm_openai
 from orchestrate.tool.assign_strategy_chain import assign_strategy_chain
-from orchestrate.tool.assign_research_chain import assign_research_chain
 from orchestrate.tool.assign_recommend_chain import assign_recommend_chain
 from shared.helper.read_text import read_text
 
@@ -17,7 +16,6 @@ orchestrate_agent = create_react_agent(
     tools = [
         assign_strategy_chain,
         assign_recommend_chain,
-        assign_research_chain,
     ],
     prompt = read_text("orchestrate/prompt/orchestrate_prompt.txt")
 )
